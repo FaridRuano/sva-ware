cfd32977344b3e5bac8901b97aac253167dae9fc
FaridRuano <fruanocm2777@gmail.com>
2024-11-03 21:19:48 -0500

    Update Cap 2

--- data/courses.jsx
+++ data/courses.jsx
@@ -36,10 +36,10 @@ export const courses = {
                     _id: 201,
                     title: 'Introduccion',
                     content: [
-                        'Adobe Photoshop es una de las herramientas de edición de imágenes más populares y potentes del mundo. Utilizado por diseñadores, fotógrafos, artistas y creadores de contenido, Photoshop permite realizar desde simples retoques hasta complejas composiciones digitales. Su interfaz intuitiva y amplia gama de herramientas lo convierten en una plataforma ideal para aquellos que buscan mejorar sus habilidades en edición de imágenes.',
-                        'Entre las funcionalidades de Photoshop se encuentran el ajuste de colores, la corrección de imperfecciones, el uso de capas para trabajar de manera no destructiva y la capacidad de crear efectos especiales. Con esta lección, aprenderás los conceptos básicos para empezar a familiarizarte con la interfaz y las herramientas principales que ofrece Photoshop, abriendo la puerta a un mundo de posibilidades creativas.'
+                        'Illustrator es un software de diseño vectorial, se ha consolidado como una herramienta esencial para artistas, diseñadores gráficos e ilustradores de todo el mundo. A diferencia de los gráficos de mapa de bits, que se basan en píxeles, los gráficos vectoriales se crean a partir de fórmulas matemáticas que permiten ampliar o reducir las ilustraciones sin perder calidad, lo cual es ideal para trabajos que requieren una precisión y versatilidad extraordinarias, como logotipos, ilustraciones, tipografías personalizadas y gráficos complejos. Con una amplia gama de herramientas, Illustrator permite a los usuarios crear desde ilustraciones detalladas hasta diseños para impresión, interfaces digitales y mucho más, destacándose por su capacidad para adaptarse a diversas aplicaciones creativas y profesionales.',
+                        'Una de las características más notables de Illustrator es su facilidad para integrarse con otros programas de Adobe, como Photoshop, InDesign y After Effects, lo que permite un flujo de trabajo eficiente y coherente entre diferentes proyectos de diseño. Estas capacidades convierten a Illustrator en una herramienta imprescindible para quienes buscan crear gráficos de alta calidad, ya que no solo ofrece control y flexibilidad, sino que también abre un sinfín de posibilidades creativas en el mundo del diseño digital.'
                     ],
-                    videoUrl: 'sva-assets/courses/introductiontoadobesuite/cap1lesson1',
+                    videoUrl: 'sva-assets/courses/introductiontoadobesuite/cap2lesson1',
                 },
                 {
                     _id: 202,
@@ -48,7 +48,7 @@ export const courses = {
                         'La interfaz de Photoshop está diseñada para proporcionar un acceso rápido y organizado a las herramientas y funcionalidades necesarias para la edición de imágenes. En la pantalla principal, podrás ver varias áreas clave como la barra de herramientas a la izquierda, las opciones de herramienta en la parte superior, y el panel de capas a la derecha. Cada elemento cumple una función específica y te ayudará a navegar y trabajar de manera más eficiente en tus proyectos.',
                         'Las herramientas de Photoshop están agrupadas en la barra de herramientas, desde las de selección y recorte hasta las de dibujo y edición. En esta lección, exploraremos algunas de las herramientas más básicas, como la herramienta de selección rápida, el pincel y la herramienta de texto. Cada una de estas herramientas tiene configuraciones específicas que pueden ajustarse en la barra superior, lo que te permitirá personalizar tus acciones y lograr resultados más precisos en tus ediciones.'
                     ],
-                    videoUrl: 'sva-assets/courses/introductiontoadobesuite/cap1lesson1',
+                    videoUrl: 'sva-assets/courses/introductiontoadobesuite/cap2lesson1',
                 }
             ]
         },
